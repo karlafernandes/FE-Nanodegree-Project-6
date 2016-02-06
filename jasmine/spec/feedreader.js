9bc4bc9949d48f51568162f11b36e3fc8494c736
Karla Fernandes <karla@vitaminak.com.br>
2016-02-06 19:11:35 +0200

    test: new test suite named "New Feed Selection"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -103,9 +103,11 @@ $(function() {
 			expect($entries.length).toBeGreaterThan(0);
 			done();
 		});
+    });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
-
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('Initial Entries', function() {
+	
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
